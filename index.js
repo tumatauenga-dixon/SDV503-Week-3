043fc1c1bf99617a70bdf43b536bd7fe7fc9f473
Tumatauenga DIxon <tuma-dixon@live.nmit.ac.nz>
2020-03-09 14:37:51 +1300

    Added a lot

--- index.js
+++ index.js
@@ -63,11 +63,7 @@ console.log(b--);
 console.log(b);
 console.log(b--);
 
-//Increment/ Decrement Operator inside an expression
-
-let a = 1;
-console.log(5 * ++a); //10let a = 1;
-console.log(5 * a++); // 5, because a returns the previous value
+///////
 
 let o = 5;
 let p = o;
@@ -83,3 +79,44 @@ console.log("Post-Increment");
 p = o++;
 console.log(o);
 console.log(p);
+
+//In the case of pre-decrement operators, the value decrements and then 
+//the expression is executed. However in the case of post-decrement 
+//opperators, the value's expression is evaluated first and then 
+//decremented.
+
+for(let i = 0; i<=10; i++)
+{
+    console.log(i);
+}
+
+//Increment/ Decrement Operator inside an expression
+
+let a = 1;
+console.log(5 * ++a); //10let a = 1;
+console.log(5 * a++); // 5, because a returns the previous value
+
+console.log("Pre-Increment");
+let z = 1;
+console.log(z);
+console.log(10 * ++z);
+console.log(z);
+
+console.log("Post-Increment");
+let n = 1;
+console.log(n);
+console.log(10 * n++);
+console.log(n);
+
+//“Unary”, “Binary” Operators
+console.log("Unary Operator");
+let un = 1;
+un = -un;
+console.log(un);     //-1, Here the operator is unary minus
+
+console.log("Binary Operator");
+let bi = 10;
+let bi2 = 5;
+console.log(bi + bi2);  // 15, binary plus adds the value
+
+//String Concatenation
